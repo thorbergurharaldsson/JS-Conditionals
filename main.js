42ba460aeb11a03614ccf89c6ec27da24c9ed39a
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2021-09-17 07:34:37 +0000

    wrote javascript

--- main.js
+++ main.js
@@ -1,52 +1,129 @@
-// Get the current year to be able to calculate users age
-const currentYear = (new Date()).getFullYear();
-
-/*
+// An array of objects, each object is information about each person
 let people = [
-    {
-        "name": "Valina Morston",
-        "dateOfBirth": new Date('1994-4-10'),
-        "country": "Ireland",
-        "programKnowlage": false
-    }
-]
-*/
+  {
+    name: "Valina Morston",
+    birthday: "1994/4/10",
+    Children: 1,
+    Country: "Ireland",
+    programKnowledge: false,
+  },
+  {
+    name: "Gilberte Mum",
+    birthday: "1981/9/20",
+    Children: 1,
+    Country: "China",
+    programKnowledge: false,
+  },
+  {
+    name: "Faustina Slimon",
+    birthday: "1979/3/24",
+    Children: 1,
+    Country: "Cuba",
+    programKnowledge: false,
+  },
+  {
+    name: "Dolf Hollingsby",
+    birthday: "1975/6/26",
+    Children: 1,
+    Country: "Philippines",
+    programKnowledge: true,
+  },
+  {
+    name: "Orelia Christofides",
+    birthday: "1991/6/28",
+    Children: 0,
+    Country: "Canada",
+    programKnowledge: true,
+  },
+];
 
+// Get the current year to be able to calculate users age
+const currentYear = new Date().getFullYear();
 
+// Getting first persons age by getting the birthyear and subtrackting current year
+let firstPersonBirthday = people[0].birthday.slice(0, 4); // get first 4 letters of people[i].birthday
+let firstPersonAge = currentYear - firstPersonBirthday;
 
+// Getting last persons age by getting the birthyear and subtrackting current year
+// Using the at() function to be able to count backwards with a negative integer
+let lastPersonBirthday = people.at(-1).birthday.slice(0, 4); // get first 4 letters of people[i].birthday
+let lastPersonAge = currentYear - lastPersonBirthday;
 
+// If first person is older then last person
+if (firstPersonAge > lastPersonAge) {
+  console.log(
+    people[0].name +
+      " (1st person) is older then " +
+      person.at(-1).name +
+      " (last person)"
+  );
+}
+// If first person is the same age as last person
+else if (firstPersonAge == lastPersonAge) {
+  console.log(
+    people[0].name +
+      " (1st person) is the same age as " +
+      people.at(-1).name +
+      " (last person)"
+  );
+}
+// if nether return true then the last person is older then the first person
+else {
+  console.log(
+    people[0].name +
+      " (1st person) is younger then " +
+      people.at(-1).name +
+      " (last person)"
+  );
+}
 
-//var csv is the CSV file with headers
-function csvJSON(csv){
+// checks if 2nd person and 3rd person have the same amount of children
+if (people[1].Children == people[2].Children) {
+  console.log(
+    people[1].name +
+      " (2nd person) has the same amount of children as " +
+      people[2].name +
+      " (3rd person)"
+  );
+} else {
+  console.log(
+    people[1].name +
+      " (2nd person) does not have the same amount of children as " +
+      people[2].name +
+      " (3rd person)"
+  );
+}
 
-    var lines=csv.split("\n");
-  
-    var result = [];
-  
-    // NOTE: If your columns contain commas in their values, you'll need
-    // to deal with those before doing the next step 
-    // (you might convert them to &&& or something, then covert them back later)
-    // jsfiddle showing the issue https://jsfiddle.net/
-    var headers=lines[0].split(",");
-  
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        var currentline=lines[i].split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-  
-        result.push(obj);
-  
-    }
-  
-    //return result; //JavaScript object
-    return JSON.stringify(result); //JSON
-  }
+// If both 1st and 4th person know programming then Yay!
+if (people[0].programKnowledge == true && people[3].programKnowledge == true) {
+  console.log("Yay!");
+}
+// Else Let Me Google That For You
+else {
+  console.log("LMGTFY");
+}
 
+// Checks nationality of the 2nd person and outputs an appropriate greeting
+switch (people[1].Country) {
+  case "Iceland":
+    console.log("Hæ");
+    break;
+  case "Spain":
+    console.log("Hola");
+    break;
+  case "Korea":
+    console.log("여보세요");
+    break;
 
- csvJSON("/Users/thorbergurharaldsson/Documents/Tskoli-WebDev/Module3/JS-Conditionals/People CSV.csv")
+  default:
+    console.log("Hello");
+    break;
+}
 
-  console.log(csvJSON('People\ CSV.csv'))
+// ternary to check if 2nd persons name is longer then 5 letters
+let longName =
+  people[1].name.length > 5
+    ? people[1].name + " is longer then 5 letters"
+    : people[1].name + " is 5 or fewer letters";
+
+console.log(longName); // logs the outcome of the ternary
